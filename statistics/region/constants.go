118b6c593414ec9b98403efee00f50b137d3c322
dywoq <aleks.koyf@gmail.com>
2025-07-16 21:50:51 +0300

    [statistics] add region package

---
+++ statistics/region/constants.go
@@ -0,0 +1,11 @@
+package region
+
+const (
+	Fountaine Type = "fountaine"
+	Inazuma   Type = "inazuma"
+	Liyue     Type = "liyue"
+	Mondstadt Type = "mondstadt"
+	Natlan    Type = "natlan"
+	Snezhnaya Type = "snezhnaya"
+	Sumeru    Type = "sumeru"
+)
---
+++ statistics/region/type.go
@@ -0,0 +1,3 @@
+package region
+
+type Type string
