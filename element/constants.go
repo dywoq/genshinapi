60571bdae9d3c7ddcde210bf6ed7b1b683c0b8b2
dywoq <aleks.koyf@gmail.com>
2025-07-16 21:38:27 +0300

    [global] create package element

---
+++ element/constants.go
@@ -0,0 +1,12 @@
+package element
+
+const (
+	Anemo   Type = "Anemo"
+	Cryo    Type = "Cryo"
+	Dendro  Type = "Dendro"
+	Electro Type = "Electro"
+	Geo     Type = "Geo"
+	Hydro   Type = "Hydro"
+	Pyro    Type = "Pyro"
+	None    Type = "None" // for travelers-siblings
+)
---
+++ element/type.go
@@ -0,0 +1,3 @@
+package element
+
+type Type string
