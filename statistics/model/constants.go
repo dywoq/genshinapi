098193c296cd4f45080063f6727f0c12413a479e
dywoq <aleks.koyf@gmail.com>
2025-07-17 10:48:15 +0300

    [statistics] add model package

---
+++ statistics/model/constants.go
@@ -0,0 +1,9 @@
+package model
+
+const (
+	TallMale     Model = "tall-male"
+	TallFemale   Model = "tall-female"
+	MediumMale   Model = "medium-male"
+	MediumFemale Model = "medium-female"
+	ShortFemale  Model = "short-female"
+)
