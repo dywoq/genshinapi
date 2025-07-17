d0ad5bcfb8a7fb516fcf50ff9b92c401d3b19293
dywoq <aleks.koyf@gmail.com>
2025-07-17 10:52:25 +0300

    [statistics] add kind package

---
+++ statistics/kind/constants.go
@@ -0,0 +1,7 @@
+package kind
+
+const (
+	EventExclusive                                 Kind = "event-exclusive"
+	CannotBeObtainedFromWishes                     Kind = "cannot-be-obtained-from-wishes"
+	CannotBeObtainedFromCharacterWeaponEventWishes Kind = "cannot-be-obtained-from-character-weapon-event-wishes"
+)
