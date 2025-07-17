950a8be4411c26789f997e181692153e817a2ec2
dywoq <aleks.koyf@gmail.com>
2025-07-17 11:45:24 +0300

    [character] implement factory method New

--- character/character.go
+++ character/character.go
@@ -1,6 +1,7 @@
 package character
 
 import (
+	"github.com/dywoq/genshinapi/statistics"
 	"github.com/dywoq/genshinapi/statistics/element"
 	"github.com/dywoq/genshinapi/statistics/kind"
 	"github.com/dywoq/genshinapi/statistics/model"
@@ -18,4 +19,28 @@ type Character struct {
 	Model   model.Type      `json:"model"`
 	Version version.Version `json:"version"`
 	Kind    kind.Type       `json:"kind"`
+}
+
+func New(q quality.Type, e element.Type, w weapon.Type, r region.Type, m model.Type, v version.Version, k kind.Type) (*Character, error) {
+	var strChecks = map[statistics.Checker[string]]struct{}{
+		q: {},
+		e: {},
+		w: {},
+		r: {},
+		m: {},
+		k: {},
+	}
+
+	for strCheck := range strChecks {
+		err := strCheck.Check()
+		if err != nil {
+			return &Character{}, err
+		}
+	}
+
+	if err := v.Check(); err != nil {
+		return &Character{}, err
+	}
+
+	return &Character{q, e, w, r, m, v, k}, nil
 }
