0ae50f94a92dfeec646a39a155bb3e4abbfc7f74
dywoq <aleks.koyf@gmail.com>
2025-07-23 16:50:01 +0300

    [handle.go] add handling of characters page

--- handle.go
+++ handle.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 func handle(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
@@ -9,4 +12,22 @@ func handle(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	http.ServeFile(w, r, "./static/index.html")
+}
+
+func handleCharacters(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	characters, err := readCharacters()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+
+	data, err := json.Marshal(characters)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write(data)
 }
--- main.go
+++ main.go
@@ -12,6 +12,7 @@ func main() {
 	// fmt.Println("Starting server at 8080 port")
 
 	http.HandleFunc("/", handle)
+	http.HandleFunc("/characters/", handleCharacters)
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
